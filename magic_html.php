8770d351c3dba7893bd50557c9872772f260be7a
Bazzzyan <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>
2024-11-25 10:43:48 +0200

    Update

---
+++ magic_html.php
@@ -0,0 +1,28 @@
+<?php
+
+function html($name, $content="", $a=array())
+{
+    $html="<".$name;
+    foreach ($a as $param => $value)
+    {
+        $html.=" $param='$value'";
+    }
+    $html.=">";
+    if($content!="")
+    {
+        $html.=$content."</".$name.">";
+    }
+    return $html;
+}
+
+$cfg['bgcolor']="#111111";
+$cfg['text']="#999999";
+
+$body=html("b","Testing...");
+$body=html("center",$html);
+$body=html("body",$body,$cfg);
+
+$head=html("title","Test");
+$head=html("head",$head);
+
+echo html("html",$head.$body);
